f006b42867e0dff21afcab2ce49fb69617dffe02
Bẩn <levietduc1108@gmail.com>
2019-04-12 15:42:46 +0700

    Initial Commit

---
+++ LoadData_Fromplist/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LoadData_Fromplist
+//
+//  Created by Just Kidding on 4/12/19.
+//  Copyright © 2019 Just Kidding. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
